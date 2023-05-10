e79a4f1b5a5ac1cdb3f3d84b71a7732b562677a9
Kiichiro Wang <75592388+KiichiroW@users.noreply.github.com>
2023-05-10 16:19:58 -0700

    COMMIT

--- grade.sh
+++ grade.sh
@@ -8,16 +8,26 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
-
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 
+cd student-submission
+if [[ -f "ListExamples.java" ]] 
+    then
+        echo "Correct File Located."
+    else
+        echo "File not found."
+fi 
+cd ..
+
+cd grading-area
+ls
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
 
 
-2. check if right file submitted
-     find file_path_name
-
-3. 
+javac -d grading-area/*.java
